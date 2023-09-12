b370d6e453095c82ce9c435c3b651cab7ca2b4a5
Trần Hợp Kiên <52000680@student.tdtu.edu.vn>
2023-09-12 16:54:38 +0700

    Tạo menu cho phần đăng kí

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,6 +3,10 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleXmark,
+    faEarthAsia,
+    faEllipsisVertical,
+    faFileCircleQuestion,
+    faKeyboard,
     faMagnifyingGlass,
     faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
@@ -13,8 +17,26 @@ import images from '~/assets/images/';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'Tiếng Việt',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faFileCircleQuestion} />,
+        title: 'Feedback and Help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard Shortcut',
+    },
+];
+
 function Header() {
     const [searchResults, setSearchResults] = useState([]);
     useEffect(() => {
@@ -72,6 +94,12 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
